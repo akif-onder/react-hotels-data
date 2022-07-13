3f83dbe7f1eaab5df8084768c83f400dcad2fc83
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-13 15:01:24 +0300

    ..

---
+++ src/HotelData.jsx
@@ -0,0 +1,9 @@
+export default hotels = [
+    {
+        id:1,
+        name: "Hotel One",
+        img: "../public/images/hotel1.jpg",
+        desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio perferendis molestiae laudantium qui recusandae? Eaque a ab, rem quia, debitis inventore vel repellendus totam accusamus excepturi neque quis pariatur ipsam?",
+
+    }
+]
